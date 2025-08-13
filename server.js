dcc405d7c158b67f57b6ff2a34802a2c2d343299
ashokgit18 <153657994+ashokgit18@users.noreply.github.com>
2025-08-13 12:32:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static files from the "public" folder
+app.use(express.static('public'));
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
 
